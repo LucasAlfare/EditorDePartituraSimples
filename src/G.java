fe6819490c84b0b4312a24f92bcc97f5f598273f
Lucas Sousa <god-lucas@hotmail.com>
2019-02-18 15:07:45 -0300

    criando pentagrama interagível

--- src/G.java
+++ src/G.java
@@ -1,10 +1,10 @@
 //Classe para guardar campos globais
 public class G {
 
-    //altura
+    //campos da altura
     public static final int DISTANCIA_ENTRE_LINHAS = 5;
     public static final int ESPACAMENTO_INICIAL_PENTAGRAMA = DISTANCIA_ENTRE_LINHAS * 2;
 
-    //largura
+    //campos da largura
     public static final int ESPACAMENTO_HORIZONTAL = 100;
 }
